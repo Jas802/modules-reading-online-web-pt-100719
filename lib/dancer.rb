52267c96c1a1af559ae47f1a5a51a76828122fa9
Jas Spencer <jas.spencer414@gmail.com>
2019-11-08 16:24:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,5 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  inlcude Dance
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  extend MetaDancing
 end
